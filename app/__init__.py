d837ec7b04f61ad409b77e0e973287906ef32fe3
Kristel Jabbusch <kaj12@my.fsu.edu>
2021-10-17 13:15:38 -0700

    working alongside Learn w/ notes

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,9 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # importing hello_word_bp into module so we can use it in following line
+    from .routes import hello_word_bp
+    # app's predefined function to register Blueprint
+    app.register_blueprint(hello_world_bp)
+    # we can follow this pattern & duplicate this code to create/register more
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,15 @@
 from flask import Blueprint
 
+# first arg is used to identify this Blueprint from Flask server logs
+# second arg usually always name 
+hello_word_bp = Blueprint("hello_world", __name__)
+
+# responsibility of an endpoint:
+    # match the HTTP verb & request URL of an HTTP request
+    # form an HTTP response to send back to client
+
+@hello_world_bp.route("/hello_world", methods=["GET"])
+def say_hello_world():
+    my_response_body = "Hello, world!"
+    return my_response_body
+
